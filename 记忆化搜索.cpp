211e8232033d1db0fde19e428ad42e8448a85f26
ltw <3245849061@qq.com>
2020-05-09 17:57:20 +0800

    记忆化搜索

---
+++ 记忆化搜索.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h> 
+#define max_n 1000000
+
+typedef long long ll;
+
+int keep[max_n + 5] = {0};
+
+ll get_len(ll n) {
+	if (n == 1) return 1;
+	if (n <= max_n && keep[n]) return keep[n];
+	ll ret = 0;
+	if (!(n & 1)) ret = get_len(n >> 1) + 1;
+	else ret = get_len(3 * n + 1) + 1;
+	if (n <= max_n) keep[n] = ret;
+	return ret;
+}
+
+
+int main() {
+	ll ans = 0,len = 0;
+	for (int i = 1;i <= max_n; i++) {
+		ll temp = get_len(i);
+		if (temp > len) {
+			ans = i;
+			len = temp;
+		}
+	}
+	printf("num = %lld,len == %lld.\n",ans,len);
+	
+	return 0;
+} 
